df7add7880c70e1194e5d2fd7695d29038ac11c2
Mitchell Stanton-Cook <mscook@gmail.com>
2013-08-27 14:38:24 +1000

    Fixed some serious bugs/overlooks

--- MafIO/MafIO.py
+++ MafIO/MafIO.py
@@ -11,26 +11,69 @@
 
 
 class Sequence:
+    """
+    Elements of the Sequence object:
+    
+        * src  = (strain) 
+        * start
+        * size
+        * strand
+        * srcSize
+        * text (seq)
+
+    *src:* can be database.chromosome or strain or reference
+
+    """
 
     def __init__(self, line):
-        data = line.split()
-        self.species, self.chr = data[1].split('.')
+        data = line.strip().split()
+        if len(data) != 7:
+            raise Exception('Does not conform to MAF spec')
+        try:
+            self.species, self.chr = data[1].split('.')
+        except ValueError:
+            tmp = data[1].split('.')
+            if len(tmp) == 1:
+                self.species = tmp
+                self.strain = self.species
+            else:
+                raise Exception('Does not conform to MAF spec')
         self.start = int(data[2])
         self.size = int(data[3])
         self.strand = data[4]
         self.chrsize = int(data[5])
+        self.srcSize = self.chrsize
         self.seq = data[6]
-
+        self.text = self.seq
 
 def parse(handle):
+    """
+    Parse the MAF file
+   
+    Please see: http://genome.ucsc.edu/FAQ/FAQformat#format5 for the MAF 
+    format specification
 
+    'a ' = Alignment Block Lines
+    's ' = Sequence within an alignment block
+    'i ' = information about the context of the sequence lines immediately 
+           preceeding them
+    'e ' = information about empty parts of the alignment block
+    'q ' = information about the quality of each aligned base for the 
+           species/strain
+    
+    :param handle: the file handle object to the MAF file
+
+    :returns: iterator that return a group of alignemnt sequence objects every 
+              time you call it with next()
+    """
     if not isinstance(handle, str):
         raise TypeError('Need a string for file name.')
 
     with open(handle, 'r') as fi:
-        if fi.readline()[0:5] != '##maf':
-            raise Exception('Need MAF format file.')
-
+        # Check the 1st and second record identify as a MAF
+        if not contains_header(fi.readline()):
+            if not contains_header(fi.readline()):
+                raise Exception('Need MAF format file.')
         maf_container = []
 
         for line in fi:
@@ -41,3 +84,15 @@ def parse(handle):
                 maf_container = []
             else:
                 pass
+
+
+def contains_header(line):
+    """
+    Ensure that the MAF file contains a header
+
+    Header is defined as: ##maf version=1 scoring=tba.v8 
+    """
+    rv = False
+    if line.find('##maf') != -1:
+        rv = True
+    return rv
