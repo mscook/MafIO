6007a960de0a207fa5af961dde5feb0593c739f8
Mitchell Stanton-Cook <mscook@gmail.com>
2013-08-27 12:13:12 +1000

    Packageify

---
+++ MafIO/__init__.py
@@ -0,0 +1,7 @@
+__title__        = 'MafIO'
+__version__      = '0.1'
+__description__  = "This module if for reading the MAF file" 
+__author__       = 'Jian-Long Huang (Mitchell Stanton-Cook)'
+__license__      = 'MIT'
+__author_email__ = "jianlong@ntu.edu.tw (m.stantoncook@gmail.com)"
+__url__         = 'https://github.com/jlhg/MafIO (https://github.com/mscook/MafIO')
