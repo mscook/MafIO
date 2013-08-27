19d24c0eb4ddfeb16d956efd2a39bb124a3dc062
Mitchell Stanton-Cook <mscook@gmail.com>
2013-08-27 17:35:01 +1000

    Reflect this is *my* fork

--- MafIO/__init__.py
+++ MafIO/__init__.py
@@ -1,7 +1,7 @@
 __title__        = 'MafIO'
 __version__      = '0.1'
-__description__  = "This module if for reading the MAF file" 
-__author__       = 'Jian-Long Huang (Mitchell Stanton-Cook)'
+__description__  = "This module is for reading the MAF file" 
+__author__       = 'Mitchell Stanton-Cook (Jian-Long Huang - original code)'
 __license__      = 'MIT'
-__author_email__ = "jianlong@ntu.edu.tw (m.stantoncook@gmail.com)"
-__url__         = 'https://github.com/jlhg/MafIO (https://github.com/mscook/MafIO'
+__author_email__ = "m.stantoncook@gmail.com (jianlong@ntu.edu.tw)"
+__url__          = 'https://github.com/mscook/MafIO, (https://github.com/jlhg/MafIO)'
