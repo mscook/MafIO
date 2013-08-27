dc483a3702aad766ddd657be7691c95916b3cb57
Mitchell Stanton-Cook <mscook@gmail.com>
2013-08-27 14:38:38 +1000

    Some testing

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,10 +4,7 @@ sys.path.insert(0, os.path.abspath('../MafIO'))
 
 import MafIO
 
-#with open('data/example.maf') as fin:
-#    for line in fin:
-#        print line.strip()
-
-maf = MafIO.parse('data/example.maf')                                                   
-alignment = next(maf)  # Get the next list of multiple alignments              
-print len(alignment)            
+maf = MafIO.parse('data/example.maf')
+alignment = next(maf)  # Get the next list of multiple alignments
+for e in alignment:
+    print e.seq
