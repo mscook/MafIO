bb0532709c840494f037031d8e676d3b6545ec74
Mitchell Stanton-Cook <mscook@gmail.com>
2013-08-27 13:08:34 +1000

    Fix typo

--- MafIO/__init__.py
+++ MafIO/__init__.py
@@ -4,4 +4,4 @@ __description__  = "This module if for reading the MAF file"
 __author__       = 'Jian-Long Huang (Mitchell Stanton-Cook)'
 __license__      = 'MIT'
 __author_email__ = "jianlong@ntu.edu.tw (m.stantoncook@gmail.com)"
-__url__         = 'https://github.com/jlhg/MafIO (https://github.com/mscook/MafIO')
+__url__         = 'https://github.com/jlhg/MafIO (https://github.com/mscook/MafIO'
